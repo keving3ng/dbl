35b4141efa171cb6991b24cc68f95828b205dc54
Kevin Geng <kevin.geng@quarksys.ca>
2019-06-12 16:27:09 -0400

    renamed and reorganized some files

---
+++ api/inventory/test tools/mongo_init.py
@@ -0,0 +1,15 @@
+from pymongo import MongoClient
+from pprint import pprint
+import json
+from generateProductList import generateProductList
+
+client = MongoClient("mongodb://localhost:27017")
+db = client.items
+
+data = generateProductList(256)
+
+for item in data:
+    result = db.inventory.insert_one(item)
+    print('Added item {0} as {1}'.format(item, result.inserted_id))
+
+print('finished adding items')
